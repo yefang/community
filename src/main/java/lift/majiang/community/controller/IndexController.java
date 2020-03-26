a10128f46488e417278fc1e6af34b1aadf7cdf8d
yefang <fyyefang@gmail.com>
2020-03-26 13:06:27 +0800

    add index and bootStrap

--- src/main/java/lift/majiang/community/controller/IndexController.java
+++ src/main/java/lift/majiang/community/controller/IndexController.java
@@ -10,10 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 
 @Controller
-public class HelloController {
-    @GetMapping("hello")
-    public String hello(@RequestParam(name="name") String name, Model model){
-        model.addAttribute("name",name);
-        return  "hello";
+public class IndexController {
+    @GetMapping("index")
+    public String hello(){
+        return "index";
     }
 }
